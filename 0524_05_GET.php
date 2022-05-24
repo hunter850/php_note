450a277656ffbd24cca86618e488003fd0265bd7
hunterluo <hunter850711@gmail.com>
2022-05-24 17:11:32 +0800

    form get & php echo

---
+++ 0524_05_GET.php
@@ -0,0 +1,5 @@
+<?php
+    $account = isset($_GET['account']) ? $_GET['account'] : "";
+    $password = isset($_GET['password']) ? $_GET['password'] : "";
+    printf("account: %s<br>password: %s<br>", $account, $password);
+?>
