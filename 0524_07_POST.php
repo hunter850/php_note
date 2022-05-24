e6901fe5df8fc6348b3ac2ec254fae547a794653
hunterluo <hunter850711@gmail.com>
2022-05-24 17:19:39 +0800

    form post & php echo

---
+++ 0524_07_POST.php
@@ -0,0 +1,5 @@
+<?php
+    $account = isset($_POST['account']) ? $_POST['account'] : "";
+    $password = isset($_POST['password']) ? $_POST['password'] : "";
+    printf("account: %s<br>password: %s<br>", $account, $password);
+?>
